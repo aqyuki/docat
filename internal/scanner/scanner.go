5d70481632d866329a74f69a9364cacbb3eea272
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-08 07:53:36 +0900

    feat : Added the ability to retrieve files in a folder

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -1,8 +1,30 @@
 package scanner
 
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
+
 // listAllFile return list of add file in root directory
 func listAllFiles(root string) []string {
-	return nil
+	list := make([]string,0)
+
+	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			list = append(list, path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("%+v", err)
+		return nil
+	}
+
+	return list
 }
 
 func ScanDocument(path string) error {
