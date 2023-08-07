3733459930819ba1b0eabda4ed8298ad1749cbaa
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-07 18:49:37 +0900

    feat : add the ability to parse arguments

--- commands/list.go
+++ commands/list.go
@@ -2,7 +2,9 @@ package commands
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/aqyuki/docat/internal/scanner"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,24 @@ var listCommand = &cobra.Command{
 	Long: "",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("docat list")
+
+		if len(args) > 1 {
+			return fmt.Errorf("too many args")
+		}
+
+		targetDir,err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		if len(args) == 1 {
+			targetDir = args[0]
+		}
+
+		err = scanner.ScanDocument(targetDir)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
