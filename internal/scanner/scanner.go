a5e4d79c59dbfd1be268d3b2e1a001630a0409ae
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-07 22:11:57 +0900

    chore : WIP

---
+++ internal/scanner/scanner.go
@@ -0,0 +1,10 @@
+package scanner
+
+// listAllFile return list of add file in root directory
+func listAllFiles(root string) []string {
+	return nil
+}
+
+func ScanDocument(path string) error {
+	return nil
+}
