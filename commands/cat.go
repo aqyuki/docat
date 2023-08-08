5679fc6e69446e40031a4d154d2107ccec6e50fe
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-08 14:59:56 +0900

    feat : add new subcommand

---
+++ commands/cat.go
@@ -0,0 +1,16 @@
+package commands
+
+import "github.com/spf13/cobra"
+
+var catCommand = &cobra.Command{
+	Use:"cat",
+	Short: "",
+	Long: "",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func init(){
+	rootCmd.AddCommand(catCommand)
+}
