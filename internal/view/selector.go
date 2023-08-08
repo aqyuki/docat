031e848fbaacf8ab69424b2dd42344cc17d4251e
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-08 19:44:08 +0900

    feat : add file selector

--- internal/view/selector.go
+++ internal/view/selector.go
@@ -63,3 +63,32 @@ func RunDocumentSelector() (tags.DocumentType, error) {
 		return tags.NON, nil
 	}
 }
+
+func RunFileSelector(files []string) (string, error){
+	listItems := make([]list.Item,0)
+	for _, item :=range files {
+		listItems = append(listItems, ls.ListItem(item))
+	}
+	l := list.New(listItems,ls.ItemDelegate{},ls.DefaultWidth,ls.ListHeight)
+
+	l.Title = "Please select the file whose contents you wish to review"
+
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = ls.TitleStyle
+	l.Styles.PaginationStyle = ls.PaginationStyle
+	l.Styles.HelpStyle = ls.HelpStyle
+
+	model,err := tea.NewProgram(ls.ListModel{List: l}).Run()
+	if err != nil {
+		return "",err
+	}
+
+	selected := ""
+	switch got:=model.(type) {
+	case ls.ListModel:
+		selected = got.Choice
+	}
+
+	return selected,nil
+}
