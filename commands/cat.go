4e033f94f29651b5afb9776de3c50fdfce9c5046
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-08 17:33:23 +0900

    feat : add selecting feature

--- commands/cat.go
+++ commands/cat.go
@@ -1,12 +1,42 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/aqyuki/docat/internal/scanner"
+	"github.com/aqyuki/docat/internal/view"
+	"github.com/spf13/cobra"
+)
 
 var catCommand = &cobra.Command{
 	Use:   "cat",
 	Short: "",
 	Long:  "",
 	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if len(args) > 1 {
+			return fmt.Errorf("too many args")
+		}
+
+		targetDir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		if len(args) == 1 {
+			dir, err := filepath.Abs(args[0])
+			if err != nil {
+				return err
+			}
+			targetDir = dir
+		}
+
+		files,err := scanner.CreateDocumentList(targetDir)
+		if err != nil {
+			return err
+		}
+		view.RunDocumentSelector(files)
 		return nil
 	},
 }
