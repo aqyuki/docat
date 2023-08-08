55f4632a62089e6574d922bab9cd7d04a57383aa
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-08 17:33:07 +0900

    feat : add UI selector

---
+++ internal/view/selector.go
@@ -0,0 +1,65 @@
+package view
+
+import (
+	"github.com/aqyuki/docat/internal/tags"
+	ls "github.com/aqyuki/docat/internal/view/list"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	README       = "README"
+	LICENSE      = "LICENSE"
+	CHANGELOG    = "CHANGELOG"
+	CONTRIBUTING = "CONTRIBUTING"
+	CONTRIBUTOR  = "CONTRIBUTOR"
+)
+
+var (
+	// documentSelectorListItem is a content of a Document selector
+	documentSelectorListItem = []list.Item{
+		ls.NewModelItem(README),
+		ls.NewModelItem(LICENSE),
+		ls.NewModelItem(CHANGELOG),
+		ls.NewModelItem(CONTRIBUTING),
+		ls.NewModelItem(CONTRIBUTOR),
+	}
+)
+
+func RunDocumentSelector(files []string) (tags.DocumentType, error) {
+	l := list.New(documentSelectorListItem, ls.ItemDelegate{}, ls.DefaultWidth, ls.ListHeight)
+
+	l.Title = "Please specify documents to be dispersed"
+
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = ls.TitleStyle
+	l.Styles.PaginationStyle = ls.PaginationStyle
+	l.Styles.HelpStyle = ls.HelpStyle
+
+	model, err := tea.NewProgram(ls.ListModel{List: l}).Run()
+	if err != nil {
+		return tags.NON, err
+	}
+
+	selected := ""
+	switch got := model.(type) {
+	case ls.ListModel:
+		selected = got.Choice
+	}
+
+	switch selected {
+	case README:
+		return tags.README, nil
+	case LICENSE:
+		return tags.LICENSE, nil
+	case CHANGELOG:
+		return tags.CHANGELOG, nil
+	case CONTRIBUTING:
+		return tags.CONTRIBUTING, nil
+	case CONTRIBUTOR:
+		return tags.CONTRIBUTOR, nil
+	default:
+		return tags.NON, nil
+	}
+}
