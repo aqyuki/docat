054dafd6b3e5c3e1e82a9d7958fad2e5811e1b69
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-09 09:34:29 +0900

    chore : add function comment

--- internal/utils/args.go
+++ internal/utils/args.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 )
 
+// TargetDirectoryParser parse given arguments and return targetDirectory
+// This function assumes parsing from functions placed in the commands package.
 func TargetDirectoryParser(args []string) (string, error) {
 	if len(args) > 1 {
 		return "", fmt.Errorf("too many args")
